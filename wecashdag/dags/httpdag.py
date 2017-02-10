3258e9b172ee918182da37a82426c0c0f81998f7
bluescv <bluescv@gmail.com>
2017-02-10 11:01:59 +0800

    update httpdag, add py op as task trigger.

--- wecashdag/dags/httpdag.py
+++ wecashdag/dags/httpdag.py
@@ -2,17 +2,12 @@
 Code that goes along with the Airflow tutorial located at:
 https://github.com/airbnb/airflow/blob/master/airflow/example_dags/tutorial.py
 """
-import requests
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
-
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
+import requests
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.sensors import HttpSensor
-
-from wecashdag.requests.requests import HTTPPost
 
 default_args = {
     'owner': 'airflow',
@@ -28,15 +23,19 @@ default_args = {
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
+taskid = 'testtaskid1'
 
 with DAG('httpdag', start_date=datetime(2016, 1, 1), schedule_interval='*/1 * * * *') as dag:
     (
         dag
+        >> PythonOperator(
+            task_id='python_1',
+            python_callable=lambda: do_post(taskid))
         >> HttpSensor(
             task_id='http_sensor_check',
             http_conn_id='localtest',
             endpoint='',
-            params={},
+            params={"key1":taskid},
             # response_check=lambda response: True if "Google" in response.content else False,
             response_check=lambda response: parse_get_result(response),
             poke_interval=5,
@@ -44,10 +43,13 @@ with DAG('httpdag', start_date=datetime(2016, 1, 1), schedule_interval='*/1 * * 
             dag=dag)
     )
 
-def do_post():
-    payload = {'key1': 'value1', 'key2': 'value2'}
+
+def do_post(taskid):
+    payload = {'key1': taskid, 'key2': 'value2'}
     r = requests.post("http://localhost:8000", data=payload)
     print(r.text)
+    return True
+
 
 def parse_get_result(response):
     result = response.content.decode("UTF-8")
